02e033d47aebf9d643eda315ee898f3b5d6f03a3
sanchit36 <sbhadgal_be19@thapar.edu>
2021-11-30 00:22:40 +0530

    setup.sh added theme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]\n\
+primaryColor = '#777'\n\
+backgroundColor = '#00172B'\n\
+secondaryBackgroundColor = '#0083B8'\n\
+textColor = '#f9f9f9'\n\
+font = 'sans serif'\n\
+\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
